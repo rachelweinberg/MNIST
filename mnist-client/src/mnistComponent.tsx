4dd68d005e24041d509b77a16facdb7b21047c2f
Rachel Tehila <racheltehila@Rachels-MacBook-Air.local>
2023-11-25 21:31:18 +0200

    error handling

--- mnist-client/src/mnistComponent.tsx
+++ mnist-client/src/mnistComponent.tsx
@@ -1,40 +1,42 @@
 import React, { useState } from 'react';
 import PixelArrayToImage from './components/pixelArrayToImage.js';
 const { MnistServiceClient } = require('./proto/server_def_grpc_web_pb');
-const { MnistRequest, Response } = require('./proto/server_def_pb');
+const { MnistRequest } = require('./proto/server_def_pb');
 
 
 const MnistComponent = () => {
     const [images, setImages] = useState<any>([]);
 
     const getImages = () => {
-        const client = new MnistServiceClient('http://localhost:8080');
-        const request = new MnistRequest();
-        const stream = client.getImage(request);
-
-        stream.on('data', (image) => {
-            const label = image.getLabel();
-            const pixels = image.getPixelsList();
-            setImages((prevImages) => [...prevImages, {label, pixels}]);
-        });
-
-        stream.on('error', (error) => {
-            console.error('Error from server:', error);
-          });
-      
-        stream.on('end', () => {
-            console.log('Stream ended');
-        });
-    }
-
-    const stop = () => {
-        stream.cancel();
+        try {
+            const client = new MnistServiceClient('http://localhost:8080');
+            const request = new MnistRequest();
+            const stream = client.getImage(request);
+    
+            stream.on('data', (image) => {
+                debugger
+                const label = image.getLabel();
+                const pixels = image.getPixelsList();
+                setImages((prevImages) => [...prevImages, {label, pixels}]);
+            });
+    
+            stream.on('error', (error) => {
+                debugger
+                console.error('Error from server:', error);
+              });
+          
+            stream.on('end', () => {
+                console.log('Stream ended');
+            });
+        } catch (error) {
+            alert('error getting images');
+            console.log(error);
+        }
     }
 
     return (
         <div>
-            <button onClick={getImages}>start</button>
-            <button onClick={stop}>stop</button>
+            <button onClick={getImages}>load</button>
             <div style={{display: 'flex'}}>
                 {images.map(({label, pixels}, index) => (
                     <div key={index}>
