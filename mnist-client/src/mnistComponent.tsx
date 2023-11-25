58ad5c2470bcdf904108fbc5656688cce903fe1b
Rachel Tehila <racheltehila@Rachels-MacBook-Air.local>
2023-11-25 22:28:44 +0200

    beautify

--- mnist-client/src/mnistComponent.tsx
+++ mnist-client/src/mnistComponent.tsx
@@ -1,54 +1,54 @@
 import React, { useState } from 'react';
 import PixelArrayToImage from './components/pixelArrayToImage.js';
 const { MnistServiceClient } = require('./proto/server_def_grpc_web_pb');
-const { MnistRequest, Response } = require('./proto/server_def_pb');
+const { MnistRequest } = require('./proto/server_def_pb');
 
 
 const MnistComponent = () => {
-    const [images, setImages] = useState<any>([]);
+  const [images, setImages] = useState<any>([]);
 
-    const getImages = () => {
-        try {        
-            const client = new MnistServiceClient('http://localhost:8080');
-            const request = new MnistRequest();
-            const stream = client.getImage(request);
+  const getImages = () => {
+    try {
+      const client = new MnistServiceClient('http://localhost:8080');
+      const request = new MnistRequest();
+      const stream = client.getImage(request);
 
-            stream.on('data', (image) => {
-                try {
-                    const label = image.getLabel();
-                    const pixels = image.getPixelsList();
-                    setImages((prevImages) => [...prevImages, {label, pixels}]);
-                } catch (error) {
-                    console.error(error);
-                    alert('error trying to get images, try agaun later')
-                }
-            });
-
-            stream.on('error', (error) => {
-                console.error('Error from server:', error);
-                alert('error trying to get images, try agaun later')
-            });
-        
-            stream.on('end', () => {
-                console.log('Stream ended');
-            });
+      stream.on('data', (image) => {
+        try {
+          const label = image.getLabel();
+          const pixels = image.getPixelsList();
+          setImages((prevImages) => [...prevImages, { label, pixels }]);
         } catch (error) {
-            console.log('error');
+          console.error(error);
+          alert('error trying to get images, try agaun later')
         }
-    }
+      });
 
-    return (
-        <div>
-            <button onClick={getImages}>load</button>
-            <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                {images.map(({label, pixels}, index) => (
-                    <div key={index}>
-                        <PixelArrayToImage pixelArray={pixels} width={30} height={5} />
-                    </div>
-                ))}
-            </div>
+      stream.on('error', (error) => {
+        console.error('Error from server:', error);
+        alert('error trying to get images, try agaun later')
+      });
+
+      stream.on('end', () => {
+        console.log('Stream ended');
+      });
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={getImages}>load</button>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {images.map(({ label, pixels }, index) => (
+          <div key={index}>
+            <PixelArrayToImage pixelArray={pixels} width={30} height={5} />
+          </div>
+        ))}
       </div>
-    );
+    </div>
+  );
 };
 
 export default MnistComponent;
--- mnist-service/server.js
+++ mnist-service/server.js
@@ -4,77 +4,76 @@ const PROTO_FILE = "./proto/server_def.proto";
 const fs = require('fs');
 const csv = require('csv-parser');
 
-const dataByIndex = [];
 const options = {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true,
-  };
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
 
 const pkgDefs = protoLoader.loadSync(PROTO_FILE, options);
 const mnistProto = grpc.loadPackageDefinition(pkgDefs);
 const server = new grpc.Server();
 
 server.addService(mnistProto.MnistService.service, {
-    getImage: async (call) => {
-      getData()
-        .then((images) => {
-          console.log(`getImage: call started, streaming ${images.length} images`);
-          images.forEach((image) => {
-            call.write(image);
-          });
-          call.end();
-          console.log('getImage: call ended');
-        })
-        .catch((error) => {
-          console.error('Error in getData:', error.message);
-          call.emit('error', {
-            code: grpc.status.INTERNAL,
-            details: error.message,
-          });
+  getImage: async (call) => {
+    getData()
+      .then((images) => {
+        console.log(`getImage: call started, streaming ${images.length} images`);
+        images.forEach((image) => {
+          call.write(image);
         });
-    }
+        call.end();
+        console.log('getImage: call ended');
+      })
+      .catch((error) => {
+        console.error('Error in getData:', error.message);
+        call.emit('error', {
+          code: grpc.status.INTERNAL,
+          details: error.message,
+        });
+      });
+  }
 });
 
 server.bindAsync(
-    "127.0.0.1:5000",
-    grpc.ServerCredentials.createInsecure(),
-    (error, port) => {
-      console.log(`listening on port ${port}`);
-      server.start();
-    }
-  );
+  "127.0.0.1:5000",
+  grpc.ServerCredentials.createInsecure(),
+  (error, port) => {
+    console.log(`listening on port ${port}`);
+    server.start();
+  }
+);
 
 
-  const getData = () => {
-    return new Promise((resolve, reject) => {
-      const dataByIndex = [];
-  
-      const stream = fs.createReadStream('./mnist_test.csv');
-  
-      stream
-        .on('error', (error) => {
-          reject(error); // Handle file read error
-        })
-        .pipe(csv())
-        .on('data', (row) => {
-          try {
-            const label = row.label;
-            const pixels = Object.values(row).slice(1).map(Number);
-            dataByIndex.push({ label, pixels });
-          } catch (error) {
-            // Handle row processing error
-            reject(error);
-          }
-        })
-        .on('end', () => {
-          resolve(dataByIndex);
-        })
-        .on('error', (error) => {
-          reject(error); // Handle CSV parsing error
-        });
-    });
-  };
-  
+const getData = () => {
+  return new Promise((resolve, reject) => {
+    const dataByIndex = [];
+
+    const stream = fs.createReadStream('./mnist_test.csv');
+
+    stream
+      .on('error', (error) => {
+        reject(error); // Handle file read error
+      })
+      .pipe(csv())
+      .on('data', (row) => {
+        try {
+          const label = row.label;
+          const pixels = Object.values(row).slice(1).map(Number);
+          dataByIndex.push({ label, pixels });
+        } catch (error) {
+          // Handle row processing error
+          reject(error);
+        }
+      })
+      .on('end', () => {
+        resolve(dataByIndex);
+      })
+      .on('error', (error) => {
+        reject(error); // Handle CSV parsing error
+      });
+  });
+};
+
